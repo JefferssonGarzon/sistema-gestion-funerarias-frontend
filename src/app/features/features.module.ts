17f8395c238f7620b8acf0611c130eefae8abf96
JefferssonGarzon <jeffersson.garzon@utp.edu.co>
2022-05-24 00:57:56 -0500

    instalacion de dependencias - bootstrap & angular material

---
+++ src/app/features/features.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeaturesRoutingModule } from './features-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    FeaturesRoutingModule
+  ]
+})
+export class FeaturesModule { }
