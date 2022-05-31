05c26aa2abf4766920018764c4b680f533e6125b
JefferssonGarzon <jeffersson.garzon@utp.edu.co>
2022-05-30 22:32:46 -0500

    optimizacion en el Menu de los modulos customer y employee

--- src/app/features/customer/components/customer/customer.component.ts
+++ src/app/features/customer/components/customer/customer.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  text:string;
+  path:string;
+}
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -7,6 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
+  templateMenu: MenuItem[] = [
+    {
+      text: 'Mi perfil',
+      path: '/customer/my-profile'
+    },
+    {
+      text: 'Mi planes',
+      path: '/customer/my-plans'
+    },
+    {
+      text: 'Mis servicios',
+      path: '/customer/my-burial-services'
+    },
+    {
+      text: 'Mis solicitudes',
+      path: '/customer/my-requests'
+    },
+    {
+      text: 'Cerrar sesión',
+      path: '/landing'
+    },
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/features/employee/components/employee/employee.component.ts
+++ src/app/features/employee/components/employee/employee.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  text:string;
+  path:string;
+}
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,6 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
+  templateMenu: MenuItem[] = [
+    {
+      text: 'Usuarios',
+      path: '/employee/users'
+    },
+    {
+      text: 'Planes',
+      path: '/employee/plans'
+    },
+    {
+      text: 'Productos',
+      path: '/employee/products'
+    },
+    {
+      text: 'Servicios',
+      path: '/employee/services'
+    },
+    {
+      text: 'Solicitudes',
+      path: '/employee/requests'
+    },
+    {
+      text: 'Noticas',
+      path: '/employee/news'
+    },
+    {
+      text: 'Cerrar sesion',
+      path: '/landing'
+    },
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
